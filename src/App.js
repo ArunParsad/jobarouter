fb7e2a98e780eadaa13a18d1e80fd2093dcf44a3
Arun parsad <Ptech784@gmail.com>
2023-02-06 14:43:48 +0530

    adding error element

--- src/App.js
+++ src/App.js
@@ -5,38 +5,43 @@ import {
   NavLink,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import RootLayout from "./layouts/RootLayout";
-import HelpLayout from "./layouts/HelpLayout";
-import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
-import NotFound from "./pages/NotFound";
-import Careers from "./pages/careers/Careers";
-import { careersLoader } from "./pages/careers/Careers";
+} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import Faq from './pages/help/Faq'
+import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
+import Careers from './pages/careers/Careers'
+import { careersLoader } from './pages/careers/Careers'
+import CareerDetails from './pages/careers/CareerDetails'
+import { careerDetailsLoader } from './pages/careers/CareerDetails'
+import CareersError from './pages/careers/CareerError'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
+    <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="help" element={<HelpLayout />}>
-        <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+      <Route path='/about' element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq' element={<Faq />} />
+        <Route path='contact' element={<Contact />} />
       </Route>
-      <Route
-        index
-        path="careers"
-        element={<Careers />}
-        loader={careersLoader}
-      />
-      <Route path="*" element={<NotFound />}></Route>
+      <Route path='careers' errorElement={<CareersError />}>
+        <Route index element={<Careers />} loader={careersLoader} />
+        <Route
+          path=':id'
+          loader={careerDetailsLoader}
+          element={<CareerDetails />}
+        />
+      </Route>
+      <Route path='*' element={<NotFound />}></Route>
     </Route>
   )
-);
+)
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -1,24 +1,27 @@
-import { useLoaderData, Link } from "react-router-dom";
+import { useLoaderData, Link } from 'react-router-dom'
 
 const Careers = () => {
-  const careers = useLoaderData();
+  const careers = useLoaderData()
   return (
-    <div className="careers">
+    <div className='careers'>
       {careers.map((career) => {
         return (
-          <Link key={career.id} to="/">
+          <Link key={career.id} to={career.id.toString()}>
             <p>{career.title}</p>
             <p>Based on {career.location}</p>
           </Link>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default Careers;
+export default Careers
 
 export const careersLoader = async () => {
-  const res = await fetch("http://localhost:4000/careers");
-  return res.json();
-};
+  const res = await fetch('http://localhost:4000/careers')
+  if (!res.ok) {
+    throw new Error('Could not fetch careers')
+  }
+  return res.json()
+}
