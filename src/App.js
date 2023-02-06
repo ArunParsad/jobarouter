07f46dba9f994f70c3ba94f722d146c8e4740b13
Arun parsad <Ptech784@gmail.com>
2023-02-06 06:38:06 +0530

    adding notfound page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import RootLayout from './layouts/RootLayout'
+import HelpLayout from './layouts/HelpLayout'
+import Faq from './pages/help/Faq'
+import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq' element={<Faq />}></Route>
+        <Route path='contact' element={<Contact />}></Route>
+      </Route>
+      <Route path='*' element={<NotFound />}></Route>
+    </Route>
+  )
+)
 function App() {
-  return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App
